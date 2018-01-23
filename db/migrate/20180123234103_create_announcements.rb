6a6f8cfe8645cabeeecc6400c58e63e07127b604
josepaez2 <josepaez_2@hotmail.com>
2018-01-23 18:42:46 -0500

    Creates user and announcement models

---
+++ db/migrate/20180123234103_create_announcements.rb
@@ -0,0 +1,12 @@
+class CreateAnnouncements < ActiveRecord::Migration[5.1]
+  def change
+    create_table :announcements do |t|
+      t.belongs_to :user, foreign_key: true
+      t.string :content
+      t.datetime :expiration_date
+      t.boolean :seen_state
+
+      t.timestamps
+    end
+  end
+end
