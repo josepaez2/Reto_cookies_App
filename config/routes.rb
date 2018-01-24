4588d28845fcc07992a86dca8f8d8fbf9b3c7449
josepaez2 <josepaez_2@hotmail.com>
2018-01-23 21:15:01 -0500

    Adds CSS and JS to the asset directory, removes basic user controller and generates devise's user controller and views and sets some basic routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,30 +1,39 @@
 Rails.application.routes.draw do
-  devise_for :users
-  get '/', to: 'users#index'
-  
+  devise_for :users, controllers: { sessions: 'users/sessions' }
+  # SI QUIERES VER LAS RUTAS MIRA EN RAILS ROUTES
+  root 'users#index'
   get 'announcements/index'
-
   get 'announcements/show'
-
   get 'announcements/new'
-
   get 'announcements/create'
-
   get 'announcements/destroy'
 
-  get 'users/index'
+  # For customizing routes:
+  # devise_for :users, path: 'auth', path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret', confirmation: 'verification', unlock: 'unblock', registration: 'register', sign_up: 'cmon_let_me_in' }
 
-  get 'users/show'
 
-  get 'users/edit'
+#                   Prefix Verb   URI Pattern                      Controller#Action
+#                     root GET    /                                users#index
 
-  get 'users/update'
+#         new_user_session GET    /users/sign_in(.:format)         devise/sessions#new
+#             user_session POST   /users/sign_in(.:format)         devise/sessions#create
+#     destroy_user_session DELETE /users/sign_out(.:format)        devise/sessions#destroy
+#        new_user_password GET    /users/password/new(.:format)    devise/passwords#new
+#       edit_user_password GET    /users/password/edit(.:format)   devise/passwords#edit
+#            user_password PATCH  /users/password(.:format)        devise/passwords#update
+#                          PUT    /users/password(.:format)        devise/passwords#update
+#                          POST   /users/password(.:format)        devise/passwords#create
+# cancel_user_registration GET    /users/cancel(.:format)          devise/registrations#cancel
+#    new_user_registration GET    /users/sign_up(.:format)         devise/registrations#new
+#   edit_user_registration GET    /users/edit(.:format)            devise/registrations#edit
+#        user_registration PATCH  /users(.:format)                 devise/registrations#update
+#                          PUT    /users(.:format)                 devise/registrations#update
+#                          DELETE /users(.:format)                 devise/registrations#destroy
+#                          POST   /users(.:format)                 devise/registrations#create
 
-  get 'users/new'
-
-  get 'users/create'
-
-  get 'users/destroy'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+#      announcements_index GET    /announcements/index(.:format)   announcements#index
+#       announcements_show GET    /announcements/show(.:format)    announcements#show
+#        announcements_new GET    /announcements/new(.:format)     announcements#new
+#     announcements_create GET    /announcements/create(.:format)  announcements#create
+#    announcements_destroy GET    /announcements/destroy(.:format) announcements#destroy
 end
