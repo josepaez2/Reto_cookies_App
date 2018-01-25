41790283e2a4f5d9a12c0d84b8e942773b5b5a45
josepaez2 <josepaez_2@hotmail.com>
2018-01-25 00:37:09 -0500

    Turns the views prettier, organizes announcements controller and routes.rb, adds bootstrap and foundation to the pipeline

--- app/controllers/announcements_controller.rb
+++ app/controllers/announcements_controller.rb
@@ -1,16 +1,35 @@
 class AnnouncementsController < ApplicationController
+  
   def index
+    @announcements = Announcement.all
   end
-
   def show
+    @announcement = Announcement.find(params[:id])
   end
-
   def new
+    @announcement = Announcement.new
   end
-
   def create
+    @user = current_user
+    # @announcement = Announcement.new(announcement_params)
+    @user.announcements.create(announcement_params)
+    # if @user.announcements.save
+      # log_in @user
+      # redirect_to @user
+      redirect_to action: "index", id: 5
+      flash.now[:success] = "Your announcement was published successfully!"
   end
 
-  def destroy
-  end
+  # def destroy
+  #   @room = Room.find(params[:id])
+  #   @room.destroy
+  #   redirect_to rooms_path
+  # end
+  
+  # private
+
+  # def announcement_params
+  #   params.require(:announcement).permit(:content, :expiration_date, :seen_state)
+  # end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,39 +1,42 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: { sessions: 'users/sessions' }
-  # SI QUIERES VER LAS RUTAS MIRA EN RAILS ROUTES
+
   root 'announcements#index'
-  get 'announcements/index'
-  get 'announcements/show'
-  get 'announcements/new'
-  get 'announcements/create'
-  get 'announcements/destroy'
+  resources :announcements, except: [:edit, :update, :destroy]
+  devise_for :users, controllers: { sessions: 'users/sessions' }
+
+
+  #   # For giving scope to parameters:
+  # devise_scope :user do get "/users/sessions/index" => "users/sessions#index" end
 
   # For customizing routes:
   # devise_for :users, path: 'auth', path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret', confirmation: 'verification', unlock: 'unblock', registration: 'register', sign_up: 'cmon_let_me_in' }
 
+#                 Prefix Verb   URI Pattern                      Controller#Action
+#            users_index GET    /users/sessions/index(.:format)  users/sessions#index
+  # get '/users/sessions/index', to: "users/sessions#index" , as: "users_index"
+  
+
 
 #                   Prefix Verb   URI Pattern                      Controller#Action
-#                     root GET    /                                users#index
+#                     root GET    /                              announcements#index
+#       announcement_index GET    /announcement(.:format)        announcement#index
+#                          POST   /announcement(.:format)        announcement#create
+#         new_announcement GET    /announcement/new(.:format)    announcement#new
+#             announcement GET    /announcement/:id(.:format)    announcement#show
+#         new_user_session GET    /users/sign_in(.:format)       users/sessions#new
+#             user_session POST   /users/sign_in(.:format)       users/sessions#create
+#     destroy_user_session DELETE /users/sign_out(.:format)      users/sessions#destroy
+#        new_user_password GET    /users/password/new(.:format)  devise/passwords#new
+#       edit_user_password GET    /users/password/edit(.:format) devise/passwords#edit
+#            user_password PATCH  /users/password(.:format)      devise/passwords#update
+#                          PUT    /users/password(.:format)      devise/passwords#update
+#                          POST   /users/password(.:format)      devise/passwords#create
+# cancel_user_registration GET    /users/cancel(.:format)        devise/registrations#cancel
+#    new_user_registration GET    /users/sign_up(.:format)       devise/registrations#new
+#   edit_user_registration GET    /users/edit(.:format)          devise/registrations#edit
+#        user_registration PATCH  /users(.:format)               devise/registrations#update
+#                          PUT    /users(.:format)               devise/registrations#update
+#                          DELETE /users(.:format)               devise/registrations#destroy
+#                          POST   /users(.:format)               devise/registrations#create
 
-#         new_user_session GET    /users/sign_in(.:format)         devise/sessions#new
-#             user_session POST   /users/sign_in(.:format)         devise/sessions#create
-#     destroy_user_session DELETE /users/sign_out(.:format)        devise/sessions#destroy
-#        new_user_password GET    /users/password/new(.:format)    devise/passwords#new
-#       edit_user_password GET    /users/password/edit(.:format)   devise/passwords#edit
-#            user_password PATCH  /users/password(.:format)        devise/passwords#update
-#                          PUT    /users/password(.:format)        devise/passwords#update
-#                          POST   /users/password(.:format)        devise/passwords#create
-# cancel_user_registration GET    /users/cancel(.:format)          devise/registrations#cancel
-#    new_user_registration GET    /users/sign_up(.:format)         devise/registrations#new
-#   edit_user_registration GET    /users/edit(.:format)            devise/registrations#edit
-#        user_registration PATCH  /users(.:format)                 devise/registrations#update
-#                          PUT    /users(.:format)                 devise/registrations#update
-#                          DELETE /users(.:format)                 devise/registrations#destroy
-#                          POST   /users(.:format)                 devise/registrations#create
-
-#      announcements_index GET    /announcements/index(.:format)   announcements#index
-#       announcements_show GET    /announcements/show(.:format)    announcements#show
-#        announcements_new GET    /announcements/new(.:format)     announcements#new
-#     announcements_create GET    /announcements/create(.:format)  announcements#create
-#    announcements_destroy GET    /announcements/destroy(.:format) announcements#destroy
 end
